ddb8df3d63d1a2116392d9e26fedf5f7ad8d494c
Daniel Garcia <danielgarcia@stratio.com>
2019-01-16 14:22:23 +0100

    Initial commit

---
+++ src/main/java/com/sanitas/StubRunner/StubRunnerApplication.java
@@ -0,0 +1,14 @@
+package com.sanitas.StubRunner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StubRunnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StubRunnerApplication.class, args);
+	}
+
+}
+
