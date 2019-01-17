cd1a3b0ee5e1b9f62825b33d21403cb4f65af27c
Daniel <danielgarcia@stratio.com>
2019-01-17 18:19:39 +0100

    Actualización de contrato y generación dinamica de tests

--- src/test/resources/contracts/shouldReturnFullInformationOfVisits.groovy
+++ src/test/resources/contracts/shouldReturnFullInformationOfVisits.groovy
@@ -1,27 +1,7 @@
 import groovy.json.JsonSlurper
 import org.springframework.cloud.contract.spec.Contract
 def jsonSlurper = new JsonSlurper()
-def object = jsonSlurper.parseText'''{
-    "patientInfo":{
-        "name": "Pepe",
-        "firstSurname":"Florez",
-        "secondSurname":"Garcia",
-        "gender": "Male",
-        "medicalHistory":"HXIS1233"
-    },
-    "visitStatus": "scheduled",
-    "scheduledTime": "10:10, 10/10/1010",
-    "arrivalTime": "10:10, 10/10/1010",
-    "agenda": "XDA123412",
-    "center": "Moraleja",
-    "resource": "EF Maria Gimenez Navarro",
-    "visitType": "Revision",
-    "comments": "Vive en una zona con alta contaminación",
-    "insurance": "Sanitas SA",
-    "observation": "Ha sufrido anteriormente de neumonia",
-    "visitChannel": "Videollamada",
-    "overload": ""
-}'''
+
 Contract.make {
     description "should return the full info of the visit"
     request {
@@ -31,6 +11,26 @@ Contract.make {
     }
     response {
         status 200
-        body(object)
+        body([
+                patientInfo :[
+                        name : $(p(anyNonEmptyString()), c("Pepe")),
+                        firstSurname: $(p(anyNonEmptyString()),c("Florez")),
+                        secondSurname: $(p(anyNonEmptyString()), c("Garcia")),
+                        gender:$(p(regex('(male|female)')),c("male")),
+                        medicalHistory: $(p(anyNonEmptyString()),c("XDSSI"))
+                ],
+                visitStatus : $(p(regex('(scheduled|done|canceled)')), c("scheduled")),
+                scheduledTime: $(p(anyDateTime()),c("2010-06-15T00:00:00")),
+                arrivalTime: $(p(anyDateTime()),c("2010-06-15T02:00:00")),
+                agenda: $(p(anyNonEmptyString()),c("XDD")),
+                center:$(p(anyNonEmptyString()),c("Moraleja")),
+                resource:$(p(anyNonEmptyString()),c("EF")),
+                service: "enfermeria",
+                visitType: "revision",
+                insurance: "Sanitas SA",
+                observation: "Ha sufrido anteriormente de neumonia",
+                visitChannel: $(p(regex('(videollamada|presencial|domicilio)')),c("videollamada")),
+                overload : $(p(aBoolean()),c(false))
+        ])
     }
 }
